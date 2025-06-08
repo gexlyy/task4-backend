508cd3b4349b8d24229beee167318c767dd21783
gexlyy <zhenyalevchuk@gmail.com>
2025-06-08 19:25:27 +0300

    updated backend

--- db.js
+++ db.js
@@ -1,9 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    connectionString: 'postgresql://postgres.iwmsfbcfuzgwifwpjzxc:123123@aws-0-eu-north-1.pooler.supabase.com:6543/postgres',
+    connectionString: process.env.supabase,
     ssl: { rejectUnauthorized: false }
 });
-
 
 module.exports = pool;
